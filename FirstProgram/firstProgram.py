0abe4ef0a837864ad6a50285702b8bcf1252b8fd
allenflo <107102380+azure-allen@users.noreply.github.com>
2022-09-16 16:29:48 -0700

    First Program

---
+++ FirstProgram/firstProgram.py
@@ -0,0 +1,10 @@
+# This program says hello and asks for my name
+
+print ("Hello World!")
+print ("What is your name?")
+name = input()
+print ("It is good to meet you " + name)
+print ("The lenght of your name is :" + len(name))
+print ("What is your age?")
+age = input()
+print ("You will be " + (age +1) + " in one year")
