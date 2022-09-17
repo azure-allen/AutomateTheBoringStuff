cbdab79eaf048e14f41470e1b91fb170b75a6b02
allenflo <107102380+azure-allen@users.noreply.github.com>
2022-09-16 22:18:14 -0700

    Added Guess the Random Number Game

--- FirstProgram/firstProgram.py
+++ FirstProgram/firstProgram.py
@@ -8,7 +8,9 @@ print ("The length of your name is" )
 print (len(name))
 print ("What is your age?")
 age = input()
-print ("You will be " + str(int(age) +1) + Allen" in one year")
+print ("You will be " + str(int(age) + 1) + " in one year")
+
+
 
 
 
